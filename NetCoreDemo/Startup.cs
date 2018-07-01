29dad276653a0181da91509c8feda3e2a5baea19
mlcn <molchanyuk.yuriy@gmail.com>
2018-07-01 12:11:56 +1000

    Updated startup paramerters for CORS

--- NetCoreDemo/Startup.cs
+++ NetCoreDemo/Startup.cs
@@ -7,41 +7,48 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace NetCoreDemo
 {
-	public class Startup
-	{
-		public Startup(IConfiguration configuration)
-		{
-			Configuration = configuration;
-		}
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-		public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-		public void ConfigureServices(IServiceCollection services)
-		{
-			services.AddMvc();
-			services.AddTransient<IGoogleSearchClient, SearchClient>();
-			services.AddTransient<IGoogleSearchService, SearchService>();
-		}
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddTransient<IGoogleSearchClient, SearchClient>();
+            services.AddTransient<IGoogleSearchService, SearchService>();
+            services.AddCors();
+        }
 
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseBrowserLink();
-				app.UseDeveloperExceptionPage();
-			}
-			else
-			{
-				app.UseExceptionHandler("/Error");
-			}
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
 
-			app.UseStaticFiles();
-			app.UseMvc(routes =>
-			{
-				routes.MapRoute(
-					name: "default",
-					template: "{controller=MVCDemo}/{action=Index}");
-			});
-		}
-	}
+            app.UseStaticFiles();
+            app.UseCors(
+                options => options.WithOrigins("http://localhost:3000")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+            );
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=MVCDemo}/{action=Index}");
+            });
+        }
+    }
 }
